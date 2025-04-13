6020ee8c7669b663b6cf9b3583519acc1404b114
ryan_pek <ryan_pek@mymail.sutd.edu.sg>
2025-04-13 14:39:42 +0000

    latest production branch

--- sql_docs_v3/Wandr2_creation_v3.sql
+++ sql_docs_v3/Wandr2_creation_v3.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `wandr2` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `wandr2`;
+CREATE DATABASE  IF NOT EXISTS `wandr` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `wandr`;
 -- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: wandr2
